b0f61d28fe3e1db3bbd7c4abaab9c3c9eff16163
bosnianbyte <elvirdelic.ed@gmail.com>
2025-01-13 11:15:59 -0500

    Create print_number.c

---
+++ print_number.c
@@ -0,0 +1,13 @@
+// A program that prints a sentence
+
+#include <stdio.h>
+
+int main(void)
+{ 
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    printf("Your number: %d\n", number);
+    return 0;
+}

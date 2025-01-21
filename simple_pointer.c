509741f7a8a018a23670d9c3648de6635d6f1ad8
bosnianbyte <elvirdelic.ed@gmail.com>
2025-01-21 10:35:24 -0500

    Update simple_pointer.c

--- simple_pointer.c
+++ simple_pointer.c
@@ -5,8 +5,13 @@
 int main (void)
 {
     int number = 10;
+    int *pNumber = &number;
 
     printf("The memory address of our number is %p\n", &number);
+    printf("The value of pNumber is %p\n", pNumber);
+
     printf("The value of our number is %d\n", number);
+    printf("The value at the stored address is %d\n", *pNumber);
+
     return 0;
 }

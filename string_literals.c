fe6957d0f19798e0e628ead326b582799b0f92ba
bosnianbyte <elvirdelic.ed@gmail.com>
2025-01-22 15:47:17 -0500

    Create string_literals.c

---
+++ string_literals.c
@@ -0,0 +1,16 @@
+/* A program that showcases string literals */
+
+#include <stdio.h>
+
+int main (void)
+{
+    // Must continue at the beginning of the line if using '/'
+    printf("Hello everyone, how are \
+you?\n");
+
+    // Better method of writing a string on multiple lines
+    printf("Hello everyone, how are "
+    "you?\n");
+
+    return 0;
+}
